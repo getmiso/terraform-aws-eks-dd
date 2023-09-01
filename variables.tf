60dfabeb034ed250d490c0dd999f93ab570d8fdd
deenMuhammad <zorro199696@gmail.com>
2023-09-01 16:35:46 +0900

    Remove provider upper version check

--- variables.tf
+++ variables.tf
@@ -18,7 +18,7 @@ variable "helm_chart_name" {
 
 variable "helm_chart_version" {
   type        = string
-  default     = "0.9.2"
+  default     = "1.1.0"
   description = "Version of the Helm chart"
 }
 
--- versions.tf
+++ versions.tf
@@ -4,15 +4,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.13, < 4.0"
+      version = ">= 3.13"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 1.16.0, < 3.0.0"
+      version = ">= 1.16.0"
     }
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 1.0, < 3.1"
+      version = ">= 1.0"
     }
   }
 }
